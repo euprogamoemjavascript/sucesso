c522415d4ea0fe395fc66a2bae50f41bf49ee918
euprogamoemjavascript <euprogamoemjavascript@outlook.com>
2025-02-19 01:48:16 -0300

    Create sucesso.js

---
+++ sucesso.js
@@ -0,0 +1,10 @@
+// Redireciona automaticamente após 5 segundos
+setTimeout(function() {
+    window.location.href = "https://discord.gg/bMwedBgm";
+  }, 5000);
+  
+  // Também permite redirecionamento imediato ao clicar no botão
+  document.getElementById("redirectBtn").addEventListener("click", function() {
+    window.location.href = "https://discord.gg/bMwedBgm";
+  });
+  
